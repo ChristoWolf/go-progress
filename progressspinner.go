8a748a1c1523eeb8f2ac07e42be376be4833cfc8
ChristoWolf <christoph.n.wolf@gmail.com>
2021-08-01 17:33:35 +0200

    Refactored code to not use anti-pattern related to interfaces, as one should "accept interfaces, not return them"
    Related: #18

--- progressspinner.go
+++ progressspinner.go
@@ -9,7 +9,7 @@ import (
 )
 
 // A simple progress spinner for displaying asynchronous activity.
-type progressSpinner struct {
+type ProgressSpinner struct {
 	baseProgress
 }
 
@@ -19,13 +19,13 @@ type progressSpinner struct {
 // - they delay between visualization frames,
 //
 // - and the sink where the visualization is written to.
-func NewProgressSpinner(delay time.Duration, sink io.Writer) Progresser {
-	return &progressSpinner{baseProgress{delay, sink, make(chan struct{}), nil, sync.WaitGroup{}}}
+func NewProgressSpinner(delay time.Duration, sink io.Writer) *ProgressSpinner {
+	return &ProgressSpinner{baseProgress{delay, sink, make(chan struct{}), nil, sync.WaitGroup{}}}
 }
 
 // Starts concurrent spinner progress visualization.
 // Execution of the caller goroutine continues and progress visualization may be stopped using Stop().
-func (p *progressSpinner) Start() error {
+func (p *ProgressSpinner) Start() error {
 	if p.ticker != nil {
 		return errors.New("progress spinner has already been started and/or stopped")
 	}
@@ -36,8 +36,8 @@ func (p *progressSpinner) Start() error {
 }
 
 // Internally used method containing actual progress spinner visualization logic.
-// Should be called as a goroutine during Start() of a Progresser.
-func (p *progressSpinner) work() {
+// Called as a goroutine during Start() of a ProgressSpinner.
+func (p *ProgressSpinner) work() {
 	defer p.wg.Done()
 	fmt.Fprintf(p.sink, "-")
 	for {
